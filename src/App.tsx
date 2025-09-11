6a27bc983ae79471dc3ffe44207f33f7804a0088
Guled Abdullahi <guleskysheikh@gmail.com>
2025-09-10 23:35:06 -0400

    Fix Vercel deployment issues part3

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import FamilyTree from "./FamilyTree";
 import "./App.css";
 
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("Supabase Key:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 function App() {
   return (
     <div className="App">

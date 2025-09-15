f9cde0b7baa2dcc4fbacb1fc19b9a1061fc3f297
Guled Abdullahi <guleskysheikh@gmail.com>
2025-09-14 23:24:42 -0400

    Fix Vercel deployment issues part9

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,5 +1,6 @@
 // supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
+console.log("Env Vars:", import.meta.env)
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
